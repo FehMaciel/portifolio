62b4de154e7c11e095c7b7ffe18b951b8cf0e8d7
felippemaciel <felippemacieldeveloper@gmail.com.br>
2023-12-23 22:21:52 -0300

    Analytics, SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,8 +14,11 @@ export const metadata = {
 export default function RootLayout({children}) {
 	return (
 		<html lang='pt-br'>
-			<body className={inter.className}>{children}</body>
-			<Analytics />
+			<body className={inter.className}>
+				{children}
+				<Analytics />
+				<SpeedInsights />
+			</body>
 		</html>
 	)
 }
