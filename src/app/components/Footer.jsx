6bbb0dd00e2a5b75781b94518f4d094d02e23544
felippemaciel <felippemacieldeveloper@gmail.com.br>
2023-12-23 21:32:48 -0300

    Geração keywords e footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from "react"
+import Link from "next/link"
+import Image from "next/image"
 
 const Footer = () => {
-    return (
-        <footer className='footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
-            <div className='container p-12 flex justify-between'>
-                <span>LOGO</span>
-                <p className='text-slate-600 '>© 2023 Felippe Maciel. All rights reserved.</p>
-            </div>
-        </footer>
-    )
+	return (
+		<footer className='footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white flex justify-center'>
+			<div className='container p-12 flex justify-between flex-col items-center  md:flex-row w-[95%]'>
+				<span className='m-10 md:m-0'>
+					<Link
+						href={"/"}
+						className='text-2xl  md:text-5xl text-white font-semibold'
+					>
+						<Image
+							src='/images/LogoAbertoCor.png'
+							alt='logo'
+							className=''
+							width={150}
+							height={150}
+						/>
+					</Link>
+				</span>
+				<p className='text-slate-600 '>
+					© 2023 Felippe Maciel. All rights reserved.
+				</p>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,27 @@ import Footer from "./components/Footer"
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col bg-[#121212]'>
+			<div className='key-words fixed opacity-0'>
+				<h1>Felippe Maciel Developer</h1>
+				<h1>Felippe Maciel</h1>
+				<h1>Web Developer</h1>
+				<h1>Android Developer</h1>
+				<h1>UI Developer</h1>
+				<h1>UX Developer</h1>
+				<h1>Full Stack Developer</h1>
+				<h1>Flutter Developer</h1>
+				<h1>Python Developer</h1>
+				<h1>JavaScript Developer</h1>
+				<h1>React Developer</h1>
+				<h1>Node Developer</h1>
+				<h1>PHP Developer</h1>
+				<h1>C# Developer</h1>
+				<h1>PHP Developer</h1>
+				<h1>Criação de site</h1>
+				<h1>Criação de aplicativos</h1>
+				<h1>Criar Site</h1>
+				<h1>Criar Aplicativo</h1>
+			</div>
 			<Navbar />
 			<div className='container mx-auto w-[100%] bg-[#121212] px-10 mt-[150px] relative z-10'>
 				<HeroSection />
