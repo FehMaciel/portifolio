4e73957f5f3f9d84d3906db265391969b7cd74f6
felippemaciel <felippemacieldeveloper@gmail.com.br>
2023-12-23 22:37:15 -0300

    Window.open

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,6 @@
 "use client"
 import React from "react"
 import Image from "next/image"
-import Link from "next/link"
 import {TypeAnimation} from "react-type-animation"
 
 const HeroSection = () => {
@@ -9,6 +8,9 @@ const HeroSection = () => {
 		window.open("https://ig.me/m/felippedeveloper", "_blank")
 	}
 
+	const handleClickCv = () => {
+		window.open("https://linkedin.com/in/felippemaciel", "_blank")
+	}
 	return (
 		<section>
 			<div className='grid grid-cols-1 sm:grid-cols-12 min-h-[calc(100vh-150px)] p-10'>
@@ -56,16 +58,14 @@ const HeroSection = () => {
 						>
 							Entre em Contato
 						</button>
-						<Link
-							href='https://www.linkedin.com/in/felippe-maciel'
-							target='_blank'
+						<button
+							onClick={handleClickCv}
+							className='px-0.5 py-0.5 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary to-secondary mr-4 bg-transparent hover:bg-transparent text-white  mt-3'
 						>
-							<button className='px-0.5 py-0.5 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary to-secondary mr-4 bg-transparent hover:bg-transparent text-white  mt-3'>
-								<span className='block bg-[#121212] hover:bg-primary rounded-full px-5 py-2'>
-									Open CV
-								</span>
-							</button>
-						</Link>
+							<span className='block bg-[#121212] hover:bg-primary rounded-full px-5 py-2'>
+								Open CV
+							</span>
+						</button>
 					</div>
 				</div>
 
